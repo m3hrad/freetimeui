5893f21b415daf821278c3ffd49990e8fdf03357
mahyar <mahyaar.mohammadi@gmail.com>
2017-07-24 23:57:31 +0300

    asyntask added

--- app/src/main/java/com/bros/freetime/HomeFragment.java
+++ app/src/main/java/com/bros/freetime/HomeFragment.java
@@ -1,11 +1,26 @@
 package com.bros.freetime;
 
+import android.app.Activity;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
@@ -17,6 +32,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    private String TAG = HomeFragment.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> contactList;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     // private static final String ARG_PARAM1 = "param1";
@@ -48,6 +69,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
          //   mParam1 = getArguments().getString(ARG_PARAM1);
             //  mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,8 +80,103 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        contactList = new ArrayList<>();
+        lv = (ListView) v.findViewById(R.id.list);
+
+        new GetContacts().execute();
+
+        return v;
+
+//        Bundle b = this.getIntent().getExtras();
+//        String s = b.getString("example");
+
     }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+
+
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "https://freetime-backend-dev.herokuapp.com/friends/0/";
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("friends");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String first_name = c.getString("first_name");
+                        String last_name = c.getString("last_name");
+
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        contact.put("id", id);
+                        contact.put("first_name", first_name);
+                        contact.put("last_name", last_name);
+
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(), "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }));
+
+                }
+            }
+            else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }));
+            }
+            return null;
+        }
+
+        @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        ListAdapter adapter = new SimpleAdapter(getActivity(), contactList,
+                R.layout.list_item, new String[]{ "id","first_name", "last_name"},
+                new int[]{R.id.id, R.id.first_name, R.id.last_name});
+        lv.setAdapter(adapter);
+    }
+
+        private void runOnUiThread(Thread thread) {
+        }
+    }
+
+//    private void runOnUiThread(Runnable runnable) {
+//    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -74,7 +191,6 @@ public class HomeFragment extends Fragment {
         mListener = null;
     }
 
-
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -88,5 +204,6 @@ public class HomeFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+
     }
 }
--- app/src/main/java/com/bros/freetime/MainActivity.java
+++ app/src/main/java/com/bros/freetime/MainActivity.java
@@ -1,12 +1,28 @@
 package com.bros.freetime;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.R.attr.fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_search:
                     selectedFragment = SearchFragment.newInstance();
                     break;
+
                 case R.id.navigation_add:
                     selectedFragment = AddEventFragment.newInstance();
                     break;
@@ -48,19 +65,4 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        //Fragment selectedFragment = SearchFragment.newInstance();
-//        Fragment selectedFragment = HomeFragment.newInstance();
-//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//        transaction.replace(R.id.content, selectedFragment);
-//        transaction.commit();
-//
-//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-//    }
 }
