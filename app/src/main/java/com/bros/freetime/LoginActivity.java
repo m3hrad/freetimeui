93a01d655a9df7f06e88602292c42a998212ca89
mahyar <mahyaar.mohammadi@gmail.com>
2017-07-27 19:17:22 +0300

    login by auth0 added

--- app/src/main/java/com/bros/freetime/LoginActivity.java
+++ app/src/main/java/com/bros/freetime/LoginActivity.java
@@ -1,13 +1,76 @@
 package com.bros.freetime;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        token = (TextView) findViewById(R.id.token);
+        
+        login();
+    }
+
+    private void login() {
+        token.setText("Not logged in");
+        Auth0 auth0 = new Auth0(this);
+        auth0.setOIDCConformant(true);
+        WebAuthProvider.init(auth0)
+                .withScheme("demo")
+                .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
+                .start(LoginActivity.this, new AuthCallback() {
+                    @Override
+                    public void onFailure(@NonNull final Dialog dialog) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(final AuthenticationException exception) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull final Credentials credentials) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+//                                token.setText("Logged in: " + credentials.getAccessToken());
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+
+                            }
+                        });
+                    }
+                });
     }
 }
