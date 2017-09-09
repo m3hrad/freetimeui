06026adf629edf08c176f90dc4ecf8d427204634
mahyar <mahyaar.mohammadi@gmail.com>
2017-09-09 20:17:48 +0300

    userStatus is ready

--- app/src/main/java/com/bros/freetime/HomeFragment.java
+++ app/src/main/java/com/bros/freetime/HomeFragment.java
@@ -1,23 +1,19 @@
 package com.bros.freetime;
 
-import android.app.Activity;
-import android.app.Application;
-import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
@@ -29,14 +25,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.vision.text.Text;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -46,7 +37,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.bros.freetime.R.id.textView;
+import static com.bros.freetime.R.id.toggleButton;
 import static com.facebook.FacebookSdk.getApplicationContext;
 import static com.facebook.FacebookSdk.getCacheDir;
 import static com.google.android.gms.internal.zzahf.runOnUiThread;
@@ -66,8 +57,10 @@ public class HomeFragment extends Fragment {
     private ListView lv;
     private String userId;
     private String tokenId;
-    private String responseStr = "";
     ArrayList<HashMap<String, String>> contactList;
+    String status;
+    View v;
+    ToggleButton toggle;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -112,17 +105,106 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
-
+        v = inflater.inflate(R.layout.fragment_home, container, false);
         contactList = new ArrayList<>();
         lv = (ListView) v.findViewById(R.id.list);
+        String userStatusString = getActivity().getIntent().getStringExtra("availableStatus");
+        toggle = (ToggleButton) v.findViewById(toggleButton);
+        if(userStatusString.equals("false")) {
+            toggle.setChecked(false);
+        }
+        else if(userStatusString.equals("true")) {
+            toggle.setChecked(true);
+        }
+        else {
+            Toast.makeText(getActivity(), "Network connection error, please try again", Toast.LENGTH_LONG).show();        }
 
-            userInfoRequest();
+        userFriendInfoRequest();
+
+        toggle = (ToggleButton) v.findViewById(toggleButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                availabeStatus();
+            }
+        });
         return v;
     }
 
-    private void userInfoRequest() {
+    private void availabeStatus() {
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+        Network network = new BasicNetwork(new HurlStack());
+        RequestQueue mRequestQueue = new RequestQueue(cache, network);
+        mRequestQueue.start();
+        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        userId = getActivity().getIntent().getStringExtra("userId");
+        final String url = "https://freetime-backend-dev.herokuapp.com/user/" + userId;
+        tokenId = getActivity().getIntent().getStringExtra("tokenId");
+        if(toggle.isChecked()){
+            status = "true";}
+        else{
+            status = "false";}
+
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        try {
+                            JSONObject jsonObj = new JSONObject(String.valueOf(response));
+                            String availableString = jsonObj.getString("available");
+                            if(availableString.equals("false")) {
+                                toggle.setChecked(false);
+                                Toast.makeText(getActivity(), "You are not available now", Toast.LENGTH_SHORT).show();
+                            }
+                            else if(availableString.equals("true")) {
+                                toggle.setChecked(true);
+                                Toast.makeText(getActivity(), "You are available now", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getActivity(), "Network error, please try again", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (final JSONException e) {
+                            Log.e(TAG, "Json parsing error: " + e.getMessage());
+                            runOnUiThread(new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getActivity(), "Json parsing error: " + e.getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }));
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        // error
+                        Log.d("Error.Response", "volleyError");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("available", status);
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", tokenId);
+                return headers;
+            }
+        };
+        queue.add(putRequest);
+    }
+
+    private void userFriendInfoRequest() {
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
         Network network = new BasicNetwork(new HurlStack());
         RequestQueue mRequestQueue = new RequestQueue(cache, network);
@@ -158,6 +240,8 @@ public class HomeFragment extends Fragment {
                                         R.layout.list_item, new String[]{"id", "first_name", "last_name"},
                                         new int[]{R.id.id, R.id.first_name, R.id.last_name});
                                 lv.setAdapter(adapter);
+                                //run available method
+                                availabeStatus();
                             }
                         } catch (final JSONException e) {
                             Log.e(TAG, "Json parsing error: " + e.getMessage());
